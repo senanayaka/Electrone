899d72642d2860d3edc3ca738ad1d6ac1c7bd84b
senanayaka <senanayakaparakrama@gmail.com>
2017-01-13 10:50:45 +0530

    Add electrone test files

---
+++ index.js
@@ -0,0 +1,12 @@
+const electron = require('electron')
+const {app, BrowserWindow} = electron 
+
+
+app.on('ready',function(){
+
+var mainWindow = new BrowserWindow({
+	width : 800,
+	height : 600
+})
+mainWindow.loadURL('file://'+__dirname+'/index.html')
+})
